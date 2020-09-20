6655871b7ece445a27edbcb823120612adda884c
kubi <kubilayercikti1@gmail.com>
2020-09-20 13:59:17 +0200

    Easy Ones - 3: Random Number

---
+++ 1 - Easy Ones/3-random-number.py
@@ -0,0 +1,13 @@
+"""
+Category 1 - Easy Ones
+
+3: Create a random number
+
+The problem
+Create a random number between 0 to 10
+"""
+
+import random
+
+random_number = random.randint(0, 10)
+print(random_number)
