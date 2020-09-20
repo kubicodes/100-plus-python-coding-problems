fca412aba79f5770c7590e3afd7982ec28673627
kubi <kubilayercikti1@gmail.com>
2020-09-20 13:48:42 +0200

    Easy Ones - 1: User input to Number

---
+++ 1 - Easy Ones/1_user_input_to_number.py
@@ -0,0 +1,14 @@
+"""
+Category 1 - Easy Ones
+
+1: User input to Number
+
+The problem
+Take two inputs from the user. One will be an integer. 
+The other will be a float number. Then multiply them to display the output.
+"""
+
+input_integer = int(input("Type in an integer: "))
+input_float = float(input("Type a float:"))
+
+print("Multiplied the two inputs: " + str(input_integer*input_float))
