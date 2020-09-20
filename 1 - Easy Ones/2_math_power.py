99572a44483ec4251dd73ddef3a74c4ec4a47a76
kubi <kubilayercikti1@gmail.com>
2020-09-20 13:55:07 +0200

    Easy Ones - 2: Math Power

---
+++ 1 - Easy Ones/2_math_power.py
@@ -0,0 +1,14 @@
+"""
+Category 1 - Easy Ones
+
+2: Math Power
+
+The problem
+Take two numbers from the users. Calculate the result of second number power of the first number.
+"""
+
+base_number = int(input("Type an integer as the base number."))
+power_number = int(input("Type an integer as the power number."))
+
+solution = base_number**power_number
+print(solution)
