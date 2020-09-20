ebb3e6d4584faaba62f232f41fe4160579171025
kubi <kubilayercikti1@gmail.com>
2020-09-20 14:25:04 +0200

    Encapsulated the logic

--- 1 - Easy Ones/1_user_input_to_number.py
+++ 1 - Easy Ones/1_user_input_to_number.py
@@ -11,4 +11,10 @@ The other will be a float number. Then multiply them to display the output.
 input_integer = int(input("Type in an integer: "))
 input_float = float(input("Type a float:"))
 
-print("Multiplied the two inputs: " + str(input_integer*input_float))
+"""
+Takes in two numbers as argument and returns the multiplied value of this two numbers.
+"""
+def multiply_two_numbers(number1, number2):
+    return number1*number2
+
+print("Multiplied the two inputs: " + str(multiply_two_numbers(input_integer, input_float)))
--- 1 - Easy Ones/2_math_power.py
+++ 1 - Easy Ones/2_math_power.py
@@ -10,5 +10,12 @@ Take two numbers from the users. Calculate the result of second number power of 
 base_number = int(input("Type an integer as the base number."))
 power_number = int(input("Type an integer as the power number."))
 
-solution = base_number**power_number
-print(solution)
+"""
+Takes in two numbers as argument. The first number is the base number and the second number is
+the power of the first number. It returns the result of second number power of the first number.
+"""
+
+def power_of_two_numbers(base, power):
+    return base**power
+
+print(power_of_two_numbers(base_number, power_number))
